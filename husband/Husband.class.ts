f6db6c3ad34763f4f01bfee3566663798042c23c
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-06-27 22:22:21 +0300

    feat(husband): added petCat

--- husband/Husband.class.ts
+++ husband/Husband.class.ts
@@ -1,3 +1,4 @@
+import { Cat } from '../cat/Cat.class.ts';
 import { IHouse } from '../house/House.interface.ts';
 import { Resident } from '../resident/Resident.class.ts';
 import { Log } from '../utils/Log.class.ts';
@@ -6,6 +7,7 @@ enum Action {
     Eat = 'поел',
     GoToWork = 'сходил на работу',
     PlayWoT = 'поиграл в WoT',
+    PetCat = 'погладила кота', //! Должен быть последним
 }
 
 export class Husband extends Resident {
@@ -26,7 +28,7 @@ export class Husband extends Resident {
     private _eat(): boolean {
         if (this.houseName.food >= 30) {
             this.houseName.takeFood(30);
-            // Увеличиваю счётчик съеденной еды 
+            // Увеличиваю счётчик съеденной еды
             this.houseName.incrementEatenFood(30);
             this.changeSatiety(30);
             return true;
@@ -50,9 +52,29 @@ export class Husband extends Resident {
         return true;
     }
 
+    // Поглаживание кота
+    private _petCat(): boolean {
+        this.changeHappiness(5);
+        return true;
+    }
+
     // Метод случайным образом выбирает что сегодня будет делать жена
     public randomDailyActivity(): void {
         const actions: Action[] = Object.values(Action);
+
+        // Проверка наличия кота в доме
+        const hasCat: boolean = this.houseName.residents.some((resident) =>
+            resident instanceof Cat
+        );
+
+        // Если кота нет, убираем действие PetCat из списка возможных действий
+        if (!hasCat) {
+            const index: number = actions.indexOf(Action.PetCat);
+            if (index > -1) {
+                actions.splice(index, 1);
+            }
+        }
+
         const randomIndex: number = Math.floor(Math.random() * actions.length);
         const randomAction: Action = actions[randomIndex];
 
@@ -75,6 +97,8 @@ export class Husband extends Resident {
                 return this._goToWork();
             case Action.PlayWoT:
                 return this._playWoT();
+            case Action.PetCat:
+                return this._petCat();
             default:
                 return false;
         }
