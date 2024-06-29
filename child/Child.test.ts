e564ea940e51b260db44297561718b3a055e1ae0
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-06-30 01:15:49 +0300

    test(child): created Mock Child, fixed the tests

--- child/Child.test.ts
+++ child/Child.test.ts
@@ -1,61 +1,61 @@
 import { assertStrictEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts';
-import { Child } from './Child.class.ts';
 import { MockHouse } from '../house/Mock.house.ts';
+import { MockChild } from './Mock.child.ts';
 
 Deno.test('Проверяю, что уровень сытости корректно увеличивается и уменьшается', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    child.changeSatiety(20);
-    assertStrictEquals(child.satiety, 50);
-    child.changeSatiety(-10);
-    assertStrictEquals(child.satiety, 40);
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    mockChild.changeSatiety(20);
+    assertStrictEquals(mockChild.satiety, 50);
+    mockChild.changeSatiety(-10);
+    assertStrictEquals(mockChild.satiety, 40);
 });
 
 Deno.test('Проверяю, что уровень счастья всегда 100', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    assertStrictEquals(child.happiness, 100);
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    assertStrictEquals(mockChild.happiness, 100);
 });
 
 Deno.test('Проверяю, что количество еды в доме корректно уменьшается после еды', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    const result = child['_eat']();
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    const result = mockChild['_eat']();
     assertStrictEquals(result, true);
-    assertStrictEquals(child.satiety, 50);
+    assertStrictEquals(mockChild.satiety, 50);
     assertStrictEquals(mockHouse.food, 40);
 });
 
 Deno.test('Проверяю, что ребёнок не должен есть, если еды недостаточно', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
     mockHouse.takeFood(45);
-    const result = child['_eat']();
+    const result = mockChild['_eat']();
     assertStrictEquals(result, false);
-    assertStrictEquals(child.satiety, 30);
+    assertStrictEquals(mockChild.satiety, 30);
     assertStrictEquals(mockHouse.food, 5);
 });
 
 Deno.test('Проверяю, как ребёнок спит', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    const result = child['_sleep']();
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    const result = mockChild['_sleep']();
     assertStrictEquals(result, true);
-    assertStrictEquals(child.satiety, 20);
+    assertStrictEquals(mockChild.satiety, 20);
 });
 
 Deno.test('Проверяю, как ребёнок играет', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    const result = child['_play']();
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    const result = mockChild['_play']();
     assertStrictEquals(result, true);
-    assertStrictEquals(child.satiety, 20);
+    assertStrictEquals(mockChild.satiety, 20);
 });
 
 Deno.test('Проверяю, что ребёнок может совершать случайные действия', () => {
     const mockHouse = new MockHouse('Тестовый дом');
-    const child = new Child('Тестовый ребёнок', mockHouse);
-    child.randomDailyActivity();
+    const mockChild = new MockChild('Тестовый ребёнок', mockHouse);
+    mockChild.randomDailyActivity();
     // Проверка, что уровень сытости изменился
-    assertStrictEquals(child.satiety !== 30, true);
+    assertStrictEquals(mockChild.satiety !== 30, true);
 });
