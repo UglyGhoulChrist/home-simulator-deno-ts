a61812b9089c6f2ef95ed9fe09b15d6aee29d7c7
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-06-29 21:20:55 +0300

    refactor(cat): replaced enum with methods. Fixed the daily action call

--- cat/Cat.class.ts
+++ cat/Cat.class.ts
@@ -2,27 +2,15 @@ import { IHouse } from '../house/House.interface.ts';
 import { Resident } from '../resident/Resident.class.ts';
 import { Log } from '../utils/Log.class.ts';
 
-enum Action {
-    Eat = 'поел',
-    Sleep = 'поспал',
-    ScratchWallpaper = 'подрал обои',
-}
-
 export class Cat extends Resident {
     constructor(name: string, house: IHouse) {
         super(name, house);
-    }
-
-    // Публичные методы для изменения уровня сытости
-    public changeSatiety(amount: number): void {
-        super.changeSatiety(amount);
     }
 
     // Ест
     private _eat(): boolean {
         if (this.houseName.food >= 5) {
             this.houseName.takeFood(5);
-            // Увеличиваю счётчик съеденной еды
             this.houseName.incrementEatenFood(5);
             this.changeSatiety(20);
             return true;
@@ -47,30 +35,20 @@ export class Cat extends Resident {
 
     // Случайный выбор ежедневного действия
     public randomDailyActivity(): void {
-        const actions: Action[] = Object.values(Action);
-        const randomIndex: number = Math.floor(Math.random() * actions.length);
-        const randomAction: Action = actions[randomIndex];
+        const methods = [
+            { method: this._eat, description: 'поел' },
+            { method: this._sleep, description: 'поспал' },
+            { method: this._scratchWallpaper, description: 'подрал обои' },
+        ];
+        const randomMethod =
+            methods[Math.floor(Math.random() * methods.length)];
 
         // Попытка выполнить выбранное действие
-        const actionPerformed: boolean = this.performAction(randomAction);
+        const actionPerformed: boolean = randomMethod.method.call(this);
 
         // Логирование действия, если оно выполнено успешно
         if (actionPerformed) {
-            Log.yellow(`${this.name} ${randomAction}`);
-        }
-    }
-
-    // Метод для выполнения действия на основе Enum
-    private performAction(action: Action): boolean {
-        switch (action) {
-            case Action.Eat:
-                return this._eat();
-            case Action.Sleep:
-                return this._sleep();
-            case Action.ScratchWallpaper:
-                return this._scratchWallpaper();
-            default:
-                return false;
+            Log.yellow(`${this.name} ${randomMethod.description}`);
         }
     }
 }
