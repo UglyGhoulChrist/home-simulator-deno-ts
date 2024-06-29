8e3e4dcc49192028de00179434d0bce143118524
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-06-29 21:56:56 +0300

    refactor(wife): replaced enum with methods. Fixed the daily action call

--- wife/Wife.class.ts
+++ wife/Wife.class.ts
@@ -3,30 +3,9 @@ import { IHouse } from '../house/House.interface.ts';
 import { Resident } from '../resident/Resident.class.ts';
 import { Log } from '../utils/Log.class.ts';
 
-enum Action {
-    Eat = 'поела',
-    BuyGroceries = 'купила продукты',
-    BuyFurCoat = 'купила шубу',
-    CleanHouse = 'убралась в доме',
-    PetCat = 'погладила кота', //! Должно быть последним
-}
-
 export class Wife extends Resident {
     constructor(name: string, house: IHouse) {
         super(name, house);
-    }
-
-    // Публичные методы для изменения уровня сытости и счастья
-    public changeSatiety(amount: number): void {
-        super.changeSatiety(amount);
-    }
-
-    public changeHappiness(amount: number): void {
-        super.changeHappiness(amount);
-    }
-
-    public checkSatietyAndHappiness(): void {
-        super.checkSatietyAndHappiness();
     }
 
     // Ест
@@ -90,49 +69,36 @@ export class Wife extends Resident {
 
     // Случайный выбор ежедневного действия
     public randomDailyActivity(): void {
-        const actions: Action[] = Object.values(Action);
+        const methods = [
+            { method: this._eat, description: 'поел' },
+            { method: this._buyGroceries, description: 'купила продукты' },
+            { method: this._buyFurCoat, description: 'купила шубу' },
+            { method: this._cleanHouse, description: 'убралась в доме' },
+        ];
 
         // Проверка наличия кота в доме
         const hasCat: boolean = this.houseName.residents.some((resident) =>
             resident instanceof Cat
         );
 
-        // Если кота нет, убираем действие PetCat из списка возможных действий
-        if (!hasCat) {
-            const index: number = actions.indexOf(Action.PetCat);
-            if (index > -1) {
-                actions.splice(index, 1);
-            }
+        // Если кот есть, добавляем действие PetCat в список возможных действий
+        if (hasCat) {
+            methods.push({
+                method: this._petCat,
+                description: 'погладил(а) кота',
+            });
         }
 
-        const randomIndex: number = Math.floor(Math.random() * actions.length);
-        const randomAction: Action = actions[randomIndex];
+        const randomMethod =
+            methods[Math.floor(Math.random() * methods.length)];
 
         // Попытка выполнить выбранное действие
-        const actionPerformed: boolean = this.performAction(randomAction);
+        const actionPerformed: boolean = randomMethod.method.call(this);
 
         // Логирование действия, если оно выполнено успешно
         if (actionPerformed) {
-            Log.magenta(`${this.name} ${randomAction}`);
+            Log.magenta(`${this.name} ${randomMethod.description}`);
             this.checkSatietyAndHappiness();
-        }
-    }
-
-    // Метод для выполнения действия на основе Enum
-    private performAction(action: Action): boolean {
-        switch (action) {
-            case Action.Eat:
-                return this._eat();
-            case Action.BuyGroceries:
-                return this._buyGroceries();
-            case Action.BuyFurCoat:
-                return this._buyFurCoat();
-            case Action.CleanHouse:
-                return this._cleanHouse();
-            case Action.PetCat:
-                return this._petCat();
-            default:
-                return false;
         }
     }
 }
