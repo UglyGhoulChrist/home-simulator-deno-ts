add59389df552123284e15eec3ecef8247e45106
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-06-30 20:27:14 +0300

    feat(wife): i made a choice of the daily weight method

--- wife/Wife.class.ts
+++ wife/Wife.class.ts
@@ -2,6 +2,10 @@ import { Cat } from '../cat/Cat.class.ts';
 import { IHouse } from '../house/House.interface.ts';
 import { Resident } from '../resident/Resident.class.ts';
 import { Log } from '../utils/Log.class.ts';
+import {
+    IWeightedMethod,
+    MethodSelector,
+} from '../utils/MethodSelector.class.ts';
 
 export class Wife extends Resident {
     constructor(name: string, house: IHouse) {
@@ -70,10 +74,22 @@ export class Wife extends Resident {
     // Случайный выбор ежедневного действия
     public randomDailyActivity(): void {
         const methods = [
-            { method: this._eat, description: 'поел' },
-            { method: this._buyGroceries, description: 'купила продукты' },
-            { method: this._buyFurCoat, description: 'купила шубу' },
-            { method: this._cleanHouse, description: 'убралась в доме' },
+            { method: this._eat, description: 'поел', weight: .2 },
+            {
+                method: this._buyGroceries,
+                description: 'купила продукты',
+                weight: .3,
+            },
+            {
+                method: this._buyFurCoat,
+                description: 'купила шубу',
+                weight: .1,
+            },
+            {
+                method: this._cleanHouse,
+                description: 'убралась в доме',
+                weight: .2,
+            },
         ];
 
         // Проверка наличия кота в доме
@@ -86,18 +102,19 @@ export class Wife extends Resident {
             methods.push({
                 method: this._petCat,
                 description: 'погладил(а) кота',
+                weight: .2,
             });
         }
 
-        const randomMethod =
-            methods[Math.floor(Math.random() * methods.length)];
+        const selectedMethod: IWeightedMethod = MethodSelector
+            .selectMethodByWeight(methods);
 
         // Попытка выполнить выбранное действие
-        const actionPerformed: boolean = randomMethod.method.call(this);
+        const actionPerformed: boolean = selectedMethod.method.call(this);
 
         // Логирование действия, если оно выполнено успешно
         if (actionPerformed) {
-            Log.magenta(`${this.name} ${randomMethod.description}`);
+            Log.magenta(`${this.name} ${selectedMethod.description}`);
             this.checkSatietyAndHappiness();
         }
     }
